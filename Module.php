7ab06721f960177a2d6115b83875d103a8a9a7f5
Kamelia3 <32525642+Kamelia3@users.noreply.github.com>
2018-12-11 01:41:13 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,10 +1,14 @@
 <?php
-namespace Form;
+namespace Omeka_s_Module_FeedImport;
 use Omeka\Module\AbstractModule;
 use Zend\View\Model\ViewModel;
 use Zend\Mvc\Controller\AbstractController;
 use Zend\View\Renderer\PhpRenderer;
-
+use Ebook\Form\ConfigForm;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
+use Zend\ModuleManager\ModuleManager;
+use Zend\ServiceManager\ServiceLocatorInterface;
 class Module extends AbstractModule
 {
     /** Module body **/
@@ -12,12 +16,38 @@ class Module extends AbstractModule
     /**
      * Get this module's configuration form.
      *
-     * @param ViewModel $view
+     * @param PhpRenderer $renderer
      * @return string
      */
     public function getConfigForm(PhpRenderer $renderer)
+    {      
+    return 'Upload file : <input type= "file" name="valider">';
+
+    }
+
+    public function addElements(Event $event)
     {
-        return '<input type= "submit" name="valider">';
+        $form = $event->getParam('form');
+        $fieldset = new Fieldset('example');
+        $fieldset->setLabel('Example Fieldset');
+
+        $fieldset->add([
+                'name' => 'example',
+                'type' => 'text',
+                'options' => [
+                    'label' => 'Example text input', // @translate
+                ],
+            ]);
+
+        $form->add($fieldset);
+    }
+    public function addFilters($event)
+    {
+        $inputFilter = $event->getParam('inputFilter');
+        $inputFilter->get('Upload File')->add([
+                    'name' => 'Upload File',
+                    'required' => false,
+                ]);
     }
 
     /**
@@ -29,5 +59,35 @@ class Module extends AbstractModule
     public function handleConfigForm(AbstractController $controller)
     {
         return true;
+    }
+
+     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+  
+        $sharedEventManager->attach(
+            'Omeka\Controller\Admin\Item',
+            'view.show.sidebar',
+            [$this, 'handleAdminViewShowSidebar']
+        );
+ 
+    }
+
+        public function handleAdminViewShowSidebar(Event $event)
+    {
+        $view = $event->getTarget();
+        $resource = $view->resource;
+        $services = $this->getServiceLocator();
+        $translator = $services->get('MvcTranslator');
+        $query = [];
+        $query['resource_type'] = $resource->resourceName();
+        $query['resource_ids'] = [$resource->id()];
+        $link = $view->hyperlink(
+            $translator->translate('Upload File'), // @translate
+            $view->url('admin/Omeka_s_Module_FeedImport/default', ['action' => 'create'], ['query' => $query])
+        );
+        echo '<div class="meta-group">'
+            . '<h4>' . $translator->translate('Upload File') . '</h4>'
+            . '<div class="value">' . $link . '</div>'
+            . '</div>';
     }
 }
